d3786f07d88012433673547f51de41d75baee621
Sara Suarez <sara.shdez@gmail.com>
2018-03-27 15:53:27 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { CarteleraPage } from '../pages/cartelera/cartelera';
+import { DetallePage } from '../pages/detalle/detalle';
+import { TrailerPage } from '../pages/trailer/trailer';
+import { ComprarEntradasPage } from '../pages/comprar-entradas/comprar-entradas';
+import { PaypalPage } from '../pages/paypal/paypal';
+import { ProximosEstrenosPage } from '../pages/proximos-estrenos/proximos-estrenos';
+import { MisEntradasPage } from '../pages/mis-entradas/mis-entradas';
+import { CriteriosBusquedaPage } from '../pages/criterios-busqueda/criterios-busqueda';
+import { EligeTuCinePage } from '../pages/elige-tu-cine/elige-tu-cine';
+import { LoginPage } from '../pages/login/login';
+import { CrearCuentaPage } from '../pages/crear-cuenta/crear-cuenta';
+
+
 import { HomePage } from '../pages/home/home';
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) navCtrl: Nav;
+    rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,5 +35,38 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  goToCartelera(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CarteleraPage);
+  }goToDetalle(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(DetallePage);
+  }goToTrailer(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(TrailerPage);
+  }goToComprarEntradas(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(ComprarEntradasPage);
+  }goToPaypal(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(PaypalPage);
+  }goToProximosEstrenos(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(ProximosEstrenosPage);
+  }goToMisEntradas(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(MisEntradasPage);
+  }goToCriteriosBusqueda(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CriteriosBusquedaPage);
+  }goToEligeTuCine(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(EligeTuCinePage);
+  }goToLogin(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(LoginPage);
+  }goToCrearCuenta(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CrearCuentaPage);
+  }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CriteriosBusquedaPage } from '../pages/criterios-busqueda/criterios-busqueda';
+import { EligeTuCinePage } from '../pages/elige-tu-cine/elige-tu-cine';
+import { CarteleraPage } from '../pages/cartelera/cartelera';
+import { DetallePage } from '../pages/detalle/detalle';
+import { ProximosEstrenosPage } from '../pages/proximos-estrenos/proximos-estrenos';
+import { MisEntradasPage } from '../pages/mis-entradas/mis-entradas';
+import { ComprarEntradasPage } from '../pages/comprar-entradas/comprar-entradas';
+import { MiPerfilPage } from '../pages/mi-perfil/mi-perfil';
+import { LoginPage } from '../pages/login/login';
+import { CrearCuentaPage } from '../pages/crear-cuenta/crear-cuenta';
+import { TrailerPage } from '../pages/trailer/trailer';
+import { PaypalPage } from '../pages/paypal/paypal';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CriteriosBusquedaPage,
+    EligeTuCinePage,
+    CarteleraPage,
+    DetallePage,
+    ProximosEstrenosPage,
+    MisEntradasPage,
+    ComprarEntradasPage,
+    MiPerfilPage,
+    LoginPage,
+    CrearCuentaPage,
+    TrailerPage,
+    PaypalPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +44,19 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CriteriosBusquedaPage,
+    EligeTuCinePage,
+    CarteleraPage,
+    DetallePage,
+    ProximosEstrenosPage,
+    MisEntradasPage,
+    ComprarEntradasPage,
+    MiPerfilPage,
+    LoginPage,
+    CrearCuentaPage,
+    TrailerPage,
+    PaypalPage
   ],
   providers: [
     StatusBar,
